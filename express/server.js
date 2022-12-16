fcd6f4cfc1a5a08a690422f5795c006e54c68c51
antonvh <antonhaninvl@gmail.com>
2022-12-16 11:56:50 +0300

    тест запасной контур

--- express/server.js
+++ express/server.js
@@ -9,32 +9,32 @@ const bodyParser = require('body-parser');
 const { default: PATHES } = require('../consts');
 const archivator = require('../utils/archivator');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
+// const router = express.Router();
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express.js!</h1>');
+//   res.end();
+// });
 
-PATHES.forEach((path) => {
-  router.get(path, (req, res) => res.send({ route: req.originalUrl }));
-});
+// PATHES.forEach((path) => {
+//   router.get(path, (req, res) => res.send({ route: req.originalUrl }));
+// });
 
 
 
-router.post('/', (req, res) => res.json({ body: req.body }));
-router.post('/cancel', (req, res) => {
-  try {
-    fs.writeFileSync('../data/archive.json', JSON.stringify({ answer: archivator(req.body) }), { encoding:'utf8',flag:'w' });
-    const archive = JSON.stringify(fs.readFileSync('../data/archive.json', 'utf-8'));
-    res.json({ body: archive });
-    res.send(201);
-  } catch {
-    res.json({ body: req.body });
-  } finally {
-    res.send(201);
-  }
-});
+// router.post('/', (req, res) => res.json({ body: req.body }));
+// router.post('/cancel', (req, res) => {
+//   try {
+//     fs.writeFileSync('../data/archive.json', JSON.stringify({ answer: archivator(req.body) }), { encoding:'utf8',flag:'w' });
+//     const archive = JSON.stringify(fs.readFileSync('../data/archive.json', 'utf-8'));
+//     res.json({ body: archive });
+//     res.send(201);
+//   } catch {
+//     res.json({ body: req.body });
+//   } finally {
+//     res.send(201);
+//   }
+// });
 
 // запасной контур
 app.use(express.json());
@@ -59,7 +59,7 @@ app.post('/cancel', (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
