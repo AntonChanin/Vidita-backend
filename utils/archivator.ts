e767fc7ad2fdd95e8f42fa1d00b42fd364c00e02
antonvh <antonhaninvl@gmail.com>
2022-12-16 12:18:17 +0300

    mocke archivator

--- utils/archivator.ts
+++ utils/archivator.ts
@@ -3,7 +3,7 @@ const fs = require('fs');
 const archive = JSON.parse(fs.readFileSync('../data/archive.json', 'utf-8'));
 
 const archivator = (newRecord: any) => {
-  return [
+  return [...archive.answer] ?? [
     ...archive.answer, ...[
       ...JSON.parse(newRecord)
     ]
