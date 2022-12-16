ea0d64b2419e253606684f96f7f7c7246d6caf83
antonvh <antonhaninvl@gmail.com>
2022-12-16 18:15:18 +0300

    finally обработчик

--- routes/cancel.route.js
+++ routes/cancel.route.js
@@ -5,13 +5,15 @@ const archiveMiddleware = require('../middleware/archive');
 const router = Router();
 
 router.post('/cancel', archiveMiddleware.single('cancel'), (req, res) => {
-    try {
-      if (req.file) {
-        res.json({ answer: [...archive.answer, ...JSON.parse(req.file).answer] });
-      }
-    } catch (error) {
-      console.error('SERVER:', error);
+  try {
+    if (req.file) {
+      res.json({ answer: [...archive.answer, ...JSON.parse(req.file).answer.map(({status, ...record }) => { status: 'archive', record })] });
     }
+  } catch (error) {
+    console.error('SERVER:', error);
+  } finally {
+    res.json({ answer: 'fail' });
+  }
 });
 
 module.exports = router;
