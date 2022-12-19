959b942b850ae4aa49ee76badc8ad5ff5e612732
antonvh <antonhaninvl@gmail.com>
2022-12-19 14:13:55 +0300

    архивация проходит

--- routes/cancel.route.js
+++ routes/cancel.route.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const CircularJSON = require('circular-json');
+const { writeFileSync } = require('fs');
 const archive = require('../data/archive.json');
 const archiveMiddleware = require('../middleware/archive');
 const archivateCommodilityDto = require('../dto/archivateCommodilityDto');
@@ -8,12 +10,18 @@ const router = Router();
 router.post('/cancel', archiveMiddleware.single('cancel'), (req, res) => {
   try {
     if (req.body) {
-      res.json({
-        answer: [
-          ...archive.answer,
-          ...[archivateCommodilityDto(req.body)].flat(),
-        ]
-      });
+      const answer = [
+        ...archive.answer,
+        ...[archivateCommodilityDto(req.body)].flat(),
+      ];
+      const result = res.json({ answer });
+      try {
+        writeFileSync('data/archive.json', JSON.stringify(CircularJSON.stringify({ answer })), { encoding:'utf8',flag:'w' });
+        console.log('Data successfully saved to disk');
+      } catch (error) {
+        console.log('An error has occurred ', error);
+      }
+      result;
     }
   } catch (error) {
     console.error('SERVER:', error);
